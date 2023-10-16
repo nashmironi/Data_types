709bdb3255b83f0ef1cd16f0d8a43f406a24d62a
ani <ani@TOGO.ani>
2023-10-16 14:19:52 +0400

    data_types

---
+++ level-0.js
@@ -0,0 +1,175 @@
+// 1 - Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+
+// var challenge="30 Days of JavaScript"
+// console.log(challenge)
+
+
+// 2 - Print the string on the browser console using console.log()
+
+// console.log(" ragaca maxsoooovs :D niiiiice");
+
+
+
+// 3 - Print the length of the string on the browser console using console.log()
+
+//    var txt="ar gikbens getamasheba";
+//    console.log(txt.length);
+
+
+// 4 - Change all the string characters to capital letters using toUpperCase() method
+
+    //  var txt="didi asoebit unda davweroo";
+    //  console.log(txt.toUpperCase());
+
+
+// 5 - Change all the string characters to lowercase letters using toLowerCase() method
+
+        // var txt="AXLA ES PATARATI UNDA";
+        // console.log(txt.toLocaleLowerCase());
+
+
+// 6 - Cut (slice) out the first word of the string using substr() or substring() method
+
+    //   var txt="slice";
+    //   console.log(txt.substring(1,3));
+
+
+// 7 - Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+        // var txt="30 Days Of JavaScript";
+        // console.log(txt.substring(2,21));
+
+
+// 8 - Check if the string contains a word 'Script' using includes() method
+     
+    //   var txt=" 'texti','script','ragaca'";
+    //   console.log(txt.includes('script'));
+
+
+
+// 9 - Split the string into an array using split() method
+
+    //    var txt="30 Days Of JavaScript";
+    //    console.log(txt.split(''));
+
+
+
+// 10 - Split the string '30 Days Of JavaScript' at the space using split() method
+
+        // var txt = "30 Days Of JavaScript";
+        // var txt_Array = txt.split(" ");
+        // console.log(txt_Array);
+
+// 11 - 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+
+        //  var txt="'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'";
+        //  var txt_Array = txt.split(", ");
+        //  console.log(txt_Array);
+
+        // array-im magra mawvala :D
+
+
+
+        
+// 12 - Change '30 Days Of JavaScript' to '30 Days Of Python' using replace() method.
+
+        // var txt="30 Days Of JavaScript";
+        // console.log(txt.replace('JavaScript','Python'));
+
+// 13 - What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+
+
+        //  var txt= "30 Days Of JavaScript";
+        //  var index = 15;
+        //  console.log("The character at index 15 is " + txt.charAt(index));
+
+        // Expected output: "The character at index 4 is q"
+
+
+
+// 14 - What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+
+        // var txt = "30 Days Of JavaScript";
+        // var charCode = txt.charCodeAt(txt.indexOf('J'));
+        // console.log("character code of J is " + charCode);
+       
+        // es mtlad kargad ver gavige :D 
+
+// 15 - Use indexOf to determine the position of the first occurrence of 'a' in 30 Days Of JavaScript
+
+        // var txt="30 Days Of JavaScript";
+        // var position=txt.indexOf("a");
+        // console.log("the position of the first occurrence of 'a' is " + position);
+
+
+// 16 - Use lastIndexOf to determine the position of the last occurrence of 'a' in 30 Days Of JavaScript.
+
+        // var txt="30 Days Of JavaScript";
+        // var position=txt.lastIndexOf("a");
+        // console.log("the position of the last occurrence of 'a' is " + position);
+
+
+// 17 - Use indexOf to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+        // var txt="You cannot end a sentence with because because because is a conjunction";
+        // var position=txt.indexOf("because");
+        // console.log("the position of the first occurrence of the word 'because' is " + position);
+ 
+
+
+// 18 - Use lastIndexOf to find the position of the last occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+        // var txt="You cannot end a sentence with because because because is a conjunction";
+        // var position=txt.lastIndexOf("because");
+        // console.log("the  last position of the first occurrence of the word 'because' is " + position);
+
+
+
+// 19 - Use search to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+        // var txt="You cannot end a sentence with because because because is a conjunction";
+        // var position=txt.search("because");
+        // console.log("the position of the first occurrence of the word 'because' is " + position);
+
+
+// 20 - Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+
+        // var txt=" 30 Days Of JavaScript ";
+        // console.log(txt.trim());
+
+// 21 - Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+
+    //    var txt="30 Days Of JavaScript";
+    //    console.log(txt.startsWith("30"));
+
+
+
+// 22 - Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+  
+    //    var txt="30 Days Of JavaScript";
+    //    console.log(txt.endsWith("JavaScript"));
+
+
+
+// 23 - Use match() method to find all the a’s in 30 Days Of JavaScript
+
+        // var txt="30 Days Of JavaScript";
+        // console.log(txt.match(/a/g));
+
+
+// 24 - Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+     
+        // var txt1="30 Days of ";
+        // var txt2="JavaScript";
+        // console.log(txt1.concat(txt2));
+
+// 25 - Use repeat() method to print 30 Days Of JavaScript 2 times
+
+
+    //    var txt="30 Days Of JavaScript. ";
+    //    console.log(txt.repeat(2));
+
+      
+
